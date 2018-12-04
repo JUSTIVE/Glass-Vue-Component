b3264073f3ab86f8ed6cdbad0266b850c21e2cc7
Ben <ben399399@naver.com>
2018-12-05 01:12:59 +0900

    git init

---
+++ src/components/glassmaterial.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="glassmaterial">
+    <div class="inner-shadow"/>
+    {{this.msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'glassmaterial',
+  props: ['msg']
+}
+</script>
+
+<style>
+.glassmaterial {
+  display: flex;
+  position: absolute;
+  align-content:center;
+  align-items: center;
+  top:30%;
+  left:30%;
+  /* background: rgb(248, 248, 252); */
+  /* background: rgba(91, 91, 190, 0.8); */
+  background: linear-gradient(to right, hsla(240, 43%, 60%, 0.8) 0%, hsla(240, 43%, 55%, 0.8) 100%);
+  width: 300px;
+  height: 300px;
+  border-radius: 15px;
+  box-shadow: 10px 5px 15px #2d2d554d;
+  color:white;
+  filter: blur(0.5px);
+  font-size: 32px;
+  text-shadow: 2px 2px 1px rgba(20,20, 20,0.2);
+  /* box-shadow: 10px 5px 15px rgba(44,44,44,0.3); */
+}
+.inner-shadow {
+  position: absolute;
+  top:15px;
+  left:15px;
+  border-radius: 15px;
+  background: rgba(91, 91, 190,0.2);
+  box-shadow: 10px 5px 5px rgba(45, 45, 85,0.3);
+  /* box-shadow: inset 3px 5px 15px rgba(255,255,255,0.2); */
+  /* background: rgba(66,66,66,0.11); */
+  filter: blur(3px);
+  width: 300px;
+  height: 300px;
+}
+</style>
