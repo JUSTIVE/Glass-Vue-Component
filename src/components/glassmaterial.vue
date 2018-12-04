895e81abc64a6545784d586c159059d19644c41b
Ben <ben399399@naver.com>
2018-12-05 01:24:36 +0900

    narrower shadow

--- src/components/glassmaterial.vue
+++ src/components/glassmaterial.vue
@@ -22,11 +22,11 @@ export default {
   left:30%;
   /* background: rgb(248, 248, 252); */
   /* background: rgba(91, 91, 190, 0.8); */
-  background: linear-gradient(to right, hsla(240, 43%, 60%, 0.8) 0%, hsla(240, 43%, 55%, 0.8) 100%);
+  background: linear-gradient(to right, hsla(240, 63%, 60%, 0.7) 0%, hsla(240, 63%, 55%, 0.6) 100%);
   width: 300px;
   height: 300px;
   border-radius: 15px;
-  box-shadow: 10px 5px 15px #2d2d554d;
+  box-shadow: 5px 5px 15px #2d2d554d;
   color:white;
   filter: blur(0.5px);
   font-size: 32px;
@@ -35,14 +35,14 @@ export default {
 }
 .inner-shadow {
   position: absolute;
-  top:15px;
-  left:15px;
+  top:10px;
+  left:10px;
   border-radius: 15px;
   background: rgba(91, 91, 190,0.2);
-  box-shadow: 10px 5px 5px rgba(45, 45, 85,0.3);
+  box-shadow: 5px 5px rgba(45, 45, 85,0.3);
   /* box-shadow: inset 3px 5px 15px rgba(255,255,255,0.2); */
   /* background: rgba(66,66,66,0.11); */
-  filter: blur(3px);
+  filter: blur(2px);
   width: 300px;
   height: 300px;
 }
